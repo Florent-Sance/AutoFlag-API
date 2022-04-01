f18cbabe13e1bb4d4b8e04e7993869245d49b073
Jaquiez <44443475+Jaquiez@users.noreply.github.com>
2022-03-31 23:43:23 -0400

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,6 +1,6 @@
 //AUTOFLAG API V.2 : AUTOMATICALLY AUTHENTICATE USERS THEN REDIRECT TO FLAG
 //Users should no longer be able to change their user identify from 'anonymous' to 'admin'
-//WE DEPLOYED A PATCH! OUR API HAS BEEN MOVED TO SERVER-SIDE! SEE OUR GIT -> 
+//WE DEPLOYED A PATCH! OUR API HAS BEEN MOVED TO SERVER-SIDE AND CLOSED SOURCE! NO MORE SECURITY ISSUES!
 document.getElementById('redir').innerText = 'Redirecting you to your flag shortly'
 setTimeout(()=>{
     window.location.replace("/flag");
