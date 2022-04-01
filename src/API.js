f7e171c84a79f1e89a0e5e1ef80af95ffc629953
Jaquiez <lmaokachow@gmail.com>
2022-03-31 21:37:17 -0400

    fixed spacing!

--- src/API.js
+++ src/API.js
@@ -2,31 +2,31 @@
 function base64url(source) {
     encodedSource = btoa(source);
     while (encodedSource.endsWith('=')) {
-      encodedSource = encodedSource.substring(0, encodedSource.length - 1)
+        encodedSource = encodedSource.substring(0, encodedSource.length - 1)
     }
     encodedSource = encodeURI(encodedSource)
     console.log(encodedSource)
     return encodedSource;
-  }
-  
-  function getSignedHMAC(unsignedToken) {
+}
+
+function getSignedHMAC(unsignedToken) {
     return new Promise((resolve, reject) => {
-      var xhr = new XMLHttpRequest()
-      xhr.open("POST", '/api/sign-hmac', true)
-  
-      //Send the proper header information along with the request
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-  
-      xhr.onreadystatechange = function () { // Call a function when the state changes.
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-          resolve(xhr.responseText)
+        var xhr = new XMLHttpRequest()
+        xhr.open("POST", '/api/sign-hmac', true)
+
+        //Send the proper header information along with the request
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+
+        xhr.onreadystatechange = function () { // Call a function when the state changes.
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                resolve(xhr.responseText)
+            }
         }
-      }
-      xhr.send(`message=${unsignedToken}`)
+        xhr.send(`message=${unsignedToken}`)
     })
-  }
-  
-  async function signToken() {
+}
+
+async function signToken() {
     header = `{"typ":"JWT","alg":"HS256"}`
     data = `{"fresh":false,"iat":1648755893,"jti":"a5139f01-3e20-446c-9627-16580f32f118","type":"access","sub":"anonymous","nbf":1648755893,"exp":1648756793}`
     unsignedToken = base64url(header) + "." + base64url(data)
@@ -35,36 +35,36 @@ function base64url(source) {
     signature = signature.replaceAll('+', '-').replaceAll('=', '')
     let JWT = unsignedToken + "." + signature
     return JWT
-  }
-  
-  function base64url(source) {
+}
+
+function base64url(source) {
     encodedSource = btoa(source);
     while (encodedSource.endsWith('=')) {
-      encodedSource = encodedSource.substring(0, encodedSource.length - 1)
+        encodedSource = encodedSource.substring(0, encodedSource.length - 1)
     }
     encodedSource = encodeURI(encodedSource)
     console.log(encodedSource)
     return encodedSource;
-  }
-  
-  function getSignedHMAC(unsignedToken) {
+}
+
+function getSignedHMAC(unsignedToken) {
     return new Promise((resolve, reject) => {
-      var xhr = new XMLHttpRequest()
-      xhr.open("POST", '/api/sign-hmac', true)
-  
-      //Send the proper header information along with the request
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-  
-      xhr.onreadystatechange = function () { // Call a function when the state changes.
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-          resolve(xhr.responseText)
+        var xhr = new XMLHttpRequest()
+        xhr.open("POST", '/api/sign-hmac', true)
+
+        //Send the proper header information along with the request
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+
+        xhr.onreadystatechange = function () { // Call a function when the state changes.
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                resolve(xhr.responseText)
+            }
         }
-      }
-      xhr.send(`message=${unsignedToken}`)
+        xhr.send(`message=${unsignedToken}`)
     })
-  }
-  
-  async function signToken() {
+}
+
+async function signToken() {
     header = `{"typ":"JWT","alg":"HS256"}`
     data = `{"fresh":false,"iat":1648755893,"jti":"a5139f01-3e20-446c-9627-16580f32f118","type":"access","sub":"anonymous","nbf":1648755893,"exp":1648756793}`
     unsignedToken = base64url(header) + "." + base64url(data)
@@ -73,13 +73,12 @@ function base64url(source) {
     signature = signature.replaceAll('+', '-').replaceAll('=', '')
     let JWT = unsignedToken + "." + signature
     document.cookie = JWT
-  }
-  
-  signToken()
-  
-  document.getElementById('redir').innerText = 'Redirecting you to your flag shortly'
-  setTimeout(()=>{
-      window.location.replace("/flag");
-  },2000)
-  
-  
+}
+
+signToken()
+
+document.getElementById('redir').innerText = 'Redirecting you to your flag shortly'
+setTimeout(() => {
+    window.location.replace("/flag");
+}, 2000)
+
